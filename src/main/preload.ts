99e302ff34562c0a5ebd745c53d29a55fccf6e00
Null-AiLAB <null.work.0000@gmail.com>
2026-07-03 18:11:15 +0900

    feat(main): preload bridge (window.api)

---
+++ src/main/preload.ts
@@ -0,0 +1,48 @@
+import { contextBridge, ipcRenderer, type IpcRendererEvent } from 'electron';
+import type { AppApi, LogLine, PlayitStatus, ServerStatus, SystemMetrics } from '../shared/types';
+
+function subscribe<T>(channel: string, cb: (p: T) => void): () => void {
+  const l = (_e: IpcRendererEvent, p: T) => cb(p);
+  ipcRenderer.on(channel, l);
+  return () => ipcRenderer.removeListener(channel, l);
+}
+
+const api: AppApi = {
+  getStatus: () => ipcRenderer.invoke('server:status'),
+  start: () => ipcRenderer.invoke('server:start'),
+  stop: () => ipcRenderer.invoke('server:stop'),
+  restart: () => ipcRenderer.invoke('server:restart'),
+  sendCommand: (c) => ipcRenderer.invoke('server:command', c),
+
+  installOrUpdate: () => ipcRenderer.invoke('setup:installOrUpdate'),
+  getInstallState: () => ipcRenderer.invoke('setup:getInstallState'),
+
+  getConfig: () => ipcRenderer.invoke('config:get'),
+  setConfig: (p) => ipcRenderer.invoke('config:set', p),
+
+  getSettings: () => ipcRenderer.invoke('settings:get'),
+  setSettings: (p) => ipcRenderer.invoke('settings:set', p),
+
+  listBackups: () => ipcRenderer.invoke('backup:list'),
+  createBackup: () => ipcRenderer.invoke('backup:create'),
+  restoreBackup: (id) => ipcRenderer.invoke('backup:restore', id),
+  openBackupsFolder: () => ipcRenderer.invoke('backup:openFolder'),
+
+  showPlayers: () => ipcRenderer.invoke('player:show'),
+
+  getPlayitStatus: () => ipcRenderer.invoke('playit:status'),
+  enablePlayit: () => ipcRenderer.invoke('playit:enable'),
+  disablePlayit: () => ipcRenderer.invoke('playit:disable'),
+  setPlayitSecret: (s) => ipcRenderer.invoke('playit:setSecret', s),
+  setPlayitAddress: (a) => ipcRenderer.invoke('playit:setAddress', a),
+
+  getLanAddress: () => ipcRenderer.invoke('system:lanAddress'),
+  openExternal: (url) => ipcRenderer.invoke('app:openExternal', url),
+
+  onLog: (cb) => subscribe<LogLine>('server:log', cb),
+  onStatus: (cb) => subscribe<ServerStatus>('server:status', cb),
+  onMetrics: (cb) => subscribe<SystemMetrics>('system:metrics', cb),
+  onPlayitStatus: (cb) => subscribe<PlayitStatus>('playit:status', cb),
+};
+
+contextBridge.exposeInMainWorld('api', api);
