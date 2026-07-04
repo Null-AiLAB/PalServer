f148872fd1aeb45cd9b9c733a2885ad6a3154d0f
Null-AiLAB <null.work.0000@gmail.com>
2026-07-04 23:01:36 +0900

    feat(main): embedded tray icon

---
+++ src/main/tray-icon.ts
@@ -0,0 +1,3 @@
+// Small emerald tray icon (16x16 PNG) embedded as a data URL.
+export const TRAY_ICON_DATA_URL =
+  'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAI0lEQVR42mNgAAKBnY3/ycEMlGiGGzJqwKgBowZQyQBKszMAugo/+6WPLPAAAAAASUVORK5CYII=';
